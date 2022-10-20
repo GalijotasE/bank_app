9ae77dcfee2b4b1b9d81575d954157e2fd533ecd
GalijotasE <galijotase@gmail.com>
2022-10-20 08:34:27 +0300

    Click here to see changes

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bank application v.0.1
+    *Updated bank database model to insert a "person" table into database.
+    
--- bank_db_model.py
+++ bank_db_model.py
@@ -1,3 +1,17 @@
-import sqlalchemy
+from sqlalchemy import column, Integer, String, Float, DateTime, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///bank.db')
+Base = declarative_base()
+
+class Person(Base):
+    __tablename__ = "Users"
+    id = column(Integer, primary_key = True)
+    first_name = column("First Name", String)
+    last_name = column("Last Name", String)
+    personal_code = column("Personal Code", Integer)
+    phone_number = column("Phone Number", String)
+
+    def __repr__(self):
+        return f"({self.id}, {self.first_name}, {self.last_name}, {self.personal_code}, {self.phone_number}"
 

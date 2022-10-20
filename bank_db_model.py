4a3a06bb9c2ebc45229ea7d70209249a46cd97ab
GalijotasE <galijotase@gmail.com>
2022-10-20 09:02:43 +0300

    Click here to see recent changes

--- bank_db_model.py
+++ bank_db_model.py
@@ -1,17 +1,32 @@
-from sqlalchemy import column, Integer, String, Float, DateTime, create_engine
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 engine = create_engine('sqlite:///bank.db')
 Base = declarative_base()
 
 class Person(Base):
     __tablename__ = "Users"
-    id = column(Integer, primary_key = True)
-    first_name = column("First Name", String)
-    last_name = column("Last Name", String)
-    personal_code = column("Personal Code", Integer)
-    phone_number = column("Phone Number", String)
+    id = Column(Integer, primary_key = True)
+    first_name = Column("First_Name", String)
+    last_name = Column("Last_Name", String)
+    personal_code = Column("Personal_Code", Integer)
+    phone_number = Column("Phone_Number", String)
+    accounts = relationship("Account", back_populates="Users")
 
     def __repr__(self):
         return f"({self.id}, {self.first_name}, {self.last_name}, {self.personal_code}, {self.phone_number}"
+
+class Account(Base):
+    __tablename__ = "Accounts"
+    id = Column(Integer, primary_key = True)
+    account_number = Column("Account_Number", Integer)
+    balance = Column("Balansas", Integer)
+    person_id = Column("person_id", Integer, ForeignKey("Users.id"))
+    person = relationship("Person", back_populates="accounts")
+    bank_id = Column("Bank_id", Integer, ForeignKey("bank.id"))
+    bank = relationship("Bank", back_populates ="Accounts")
+
+    def __repr__(self):
+        return f"({self.id}, {self.account_number}, {self.balance})"
 
